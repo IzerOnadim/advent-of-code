5e29fa547858405dfab6ac83bf1b249658aaa9fc
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-12-03 11:05:54 +0000

    Refactor solution 1

--- day3-haskell/solution1.hs
+++ day3-haskell/solution1.hs
@@ -1,6 +1,6 @@
 import Data.List.Split
 
-main = do  
+main = do
    contents <- readFile "input.txt"
    print $ getPowerCons (parseToList contents)
 
@@ -8,24 +8,21 @@ parseToList :: String -> [String]
 parseToList = filter (not . null) . splitOn "\n"
 
 getPowerCons :: [String] -> Integer
-getPowerCons []     = 0
-getPowerCons (x:xs) = getGammaEpsilon (x:xs) (replicate n 0)
+getPowerCons []       = 0
+getPowerCons xs@(x:_) = getProduct 0 0 acc (length acc - 1)
   where
-    n = length x -- Assume all binary strings are same length.  
-
-getGammaEpsilon :: [String] -> [Int] -> Integer
-getGammaEpsilon [] acc     = getProduct 0 0 acc (length acc - 1)
-getGammaEpsilon (x:xs) acc = getGammaEpsilon xs (binAdd x acc)
+    n   = length x -- Assume all binary strings are same length.
+    acc = foldl binAdd (replicate n 0) xs
 
 getProduct :: Integer -> Integer -> [Int] -> Int -> Integer
 getProduct g e []     _ = g * e
-getProduct g e (x:xs) n = getProduct g' e' xs (n - 1) 
+getProduct g e (x:xs) n = getProduct g' e' xs (n - 1)
   where
     shift = 2 ^ n
     (g', e') = if x >= 0 then (g + shift, e) else (g, e + shift)
 
-binAdd :: String -> [Int] -> [Int]
-binAdd [] []             = [] 
-binAdd ('0':xs) (ac:acs) = (ac - 1) : (binAdd xs acs)
-binAdd ('1':xs) (ac:acs) = (ac + 1) : (binAdd xs acs) 
+binAdd :: [Int] -> String -> [Int]
+binAdd [] []             = []
+binAdd (ac:acs) ('0':xs) = (ac - 1) : (binAdd acs xs)
+binAdd (ac:acs) ('1':xs) = (ac + 1) : (binAdd acs xs)
 binAdd _ _ = error "Invalid char in binary string or strings of differing length."
